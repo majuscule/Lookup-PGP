cebcdfea706ae89a283d4c1c187eff3ffa7464c5
Dylan Lloyd <dylan@dylansserver.com>
2013-01-11 00:59:00 -0500

    a better welcome message

--- lib/Lookup/PGP/Controller/Root.pm
+++ lib/Lookup/PGP/Controller/Root.pm
@@ -30,7 +30,7 @@ sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
     # Hello World
-    $c->response->body( $c->welcome_message );
+    $c->response->body( "Hello there!" );
 }
 
 =head2 default
